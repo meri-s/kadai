7fc6c1c6e84a2c8d4d38e2547f2cbe60e15126a1
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-12 17:00:36 +0900

    Create STEP_00-07,1.c

---
+++ STEP_00-07,1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+void main(void) {
+	FILE *fp;
+	char sBuf[256];
+
+	fp = fopen("STEP_00-07.txt", "r");
+	fgets(sBuf, 256, fp);
+
+	printf("%s", sBuf);
+	fclose(fp);
+}
