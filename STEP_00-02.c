efec0778e7fb81739a50554f7401abdb11b50988
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-15 13:35:33 +0900

    Create STEP_00-02.c

---
+++ STEP_00-02.c
@@ -0,0 +1,24 @@
+void main(void) {
+	int i;
+	unsigned int ui;
+	short h;
+	unsigned short uh;	
+	unsigned int ui16;
+
+	i = 2147483647;			/*int型の最大値 (10進数)*/
+	i = -2147483648;		/*int型の最小値 (10進数)*/
+
+	ui = 4294967295;		/*unsigned int型の最大値 (10進数)*/
+	ui = 0;					/*unsigned int型の最小値 (10進数)*/
+
+	h = 32767;				/*short型の最大値 (10進数)*/
+	h = -32768;				/*short型の最小値 (10進数)*/
+
+	uh = 65535;				/*unsigned short型の最大値 (10進数)*/
+	uh = 0;					/*unsugned short型の最小値 (10進数)*/
+
+	ui16 = 0xffffffff;		/*unsigned int型の最大値 (16進数)*/
+	ui16 = 0x0;				/*unsigned int型の最小値 (16進数)*/
+
+	return;
+}
