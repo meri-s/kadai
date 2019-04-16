8b21df2baea0b27cce7630fc36ee43ba56bb0a26
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-16 13:14:38 +0900

    Create STEP_00-06.c

---
+++ STEP_00-06.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(void) {
+	int i;
+	int i2;
+	char s;
+
+	i = 255;
+	i2 = 255;
+	s = "あいうえお";
+	printf("変数の値は%dです。\n", i);
+	printf("変数の値は0x%xです。\n", i2);
+	printf("変数の値は%sです。\n", s);
+
+	
+
+}
