c6730c65c39b51c03f9c615bc64429115bf3cd22
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-17 17:35:28 +0900

    Update STEP_00-08.c

--- STEP_00-08.c
+++ STEP_00-08.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 /* No.1の関数 */
 void func1(void);	/*プロトタイプ宣言*/
 
@@ -5,13 +7,16 @@ void func1(void);	/*プロトタイプ宣言*/
 int func2(void);	/*プロトタイプ宣言*/
 
 /* No.3の関数 */
-char* func3(void);	/*プロトタイプ宣言*/
+char* func3(void);		/*プロトタイプ宣言*/
 
 
 void main(void) {
-	func1();		/* No.1の関数を呼び出す */
-	func2();		/* No.2の関数を呼び出す */
-	func3();		/* No.3の関数を呼び出す */
+	int a = 0;			/* func2 戻り値確認用変数 */
+	char *b = NULL;		/* func3 戻り値確認用変数 */
+
+	func1();				/* No.1の関数を呼び出す */
+	a = func2();			/* No.2の関数を呼び出す */
+	b = func3();			/* No.3の関数を呼び出す */
 
 	return;
 }
