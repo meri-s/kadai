9f50bc9f1ccc8a93290ffe5477bdc06aa44ffb94
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-17 16:43:55 +0900

    Update STEP_00-08.c

--- STEP_00-08.c
+++ STEP_00-08.c
@@ -1,23 +1,23 @@
-/*No.1の関数*/
+/* No.1の関数 */
 void func1(void) {
 	return;
 }
 
-/*No.2の関数*/
+/* No.2の関数 */
 int func2(void) {
 	return 255;
 }
 
-/*No.3の関数*/
+/* No.3の関数 */
 char* func3(void) {
 	return "あいうえお";
 }
 
 
 void main(void) {
-	func1();		/*No.1の関数を呼び出す*/
-	func2();		/*No.2の関数を呼び出す*/
-	func3();		/*No.3の関数を呼び出す*/
+	func1();		/* No.1の関数を呼び出す */
+	func2();		/* No.2の関数を呼び出す */
+	func3();		/* No.3の関数を呼び出す */
 
 	return;
 }
