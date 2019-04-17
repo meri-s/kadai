69b98d13aaaf250f6492d770f1383a8d7b8f54e2
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-17 13:50:49 +0900

    Update STEP_00-08.c

--- STEP_00-08.c
+++ STEP_00-08.c
@@ -1,19 +1,23 @@
+/*No.1‚ÌŠÖ”*/
 void func1(void) {
 	return;
 }
 
+/*No.2‚ÌŠÖ”*/
 int func2(void) {
 	return 255;
 }
 
-char *func3(void) {
+/*No.3‚ÌŠÖ”*/
+char* func3(void) {
 	return "‚ ‚¢‚¤‚¦‚¨";
 }
 
+
 void main(void) {
-	func1();
-	func2();
-	func3();
+	func1();		/*No.1‚ÌŠÖ”‚ğŒÄ‚Ño‚·*/
+	func2();		/*No.2‚ÌŠÖ”‚ğŒÄ‚Ño‚·*/
+	func3();		/*No.3‚ÌŠÖ”‚ğŒÄ‚Ño‚·*/
 
 	return;
 }
