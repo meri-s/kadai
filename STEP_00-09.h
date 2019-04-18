050267a1faf4d1eaae5e663ed24104c538b3ab13
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-18 09:38:50 +0900

    Create STEP_00-09.h

---
+++ STEP_00-09.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void func1(int a);	/* No.1の関数 */	/* プロトタイプ宣言 */
+void func2(char a);	/* No.2の関数 */	/* プロトタイプ宣言 */
+void func3(int *p);	/* No.3の関数 */	/* プロトタイプ宣言 */
+void func4(char *p);/* No.4の関数 */	/* プロトタイプ宣言 */
